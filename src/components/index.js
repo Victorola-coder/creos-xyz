250e88944df0ed8fb14e7ee4af507186abd14f7c
Victor Olatunji <victoluolatunji@gmail.com>
2023-10-28 09:58:48 +0100

    lets cook now

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as SEO } from './seo';
 export { default as Navbar } from './navbar';
 export { default as Footer } from './footer';
+export { default as Button } from './button';
+export { default as Container } from './container';
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,67 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Logo, LogoName } from '../assets/svgs/svg';
+import { navLinks } from '../data/data';
+import { Link, NavLink } from 'react-router-dom';
+import { Button } from './';
 
 export default function Navbar() {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [isOpened, setIsOpened] = useState(false);
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    function handleClicks(e) {
+      if (navRef.current?.contains(e.target)) {
+        console.log('clicked');
+        return;
+      }
+      setIsOpened(false);
+    }
+    window.addEventListener('mousedown', handleClicks);
+    return () => {
+      window.removeEventListener('mousedown', handleClicks);
+    };
+  }, []);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <header>
       <nav className='flex flex-row rounded-[100px] border-[#E6EAE8] border-solid border-[5px] items-center justify-between lg:mx-[100px] lg:my-[60px] lg:px-[60px] py-[34px]'>
-        <figure className='flex flex-row'>
-          <Logo />
-          <LogoName />
-        </figure>
+        <Link to='/'>
+          <figure className='flex flex-row'>
+            <Logo />
+            <LogoName />
+          </figure>
+        </Link>
+
+        <ul className='items-center hidden md:gap-6 lg:gap-[60px] lg:flex'>
+          {navLinks.map((route) => (
+            <NavLink
+              target={route.external ? '_blank' : undefined}
+              rel={route.external ? 'noopener noreferrer' : undefined}
+              key={route.id}
+              to={route.path}
+            >
+              {({ isActive }) => (
+                <div
+                  className={`lg:text-base text-xs font-medium before:content-[''] before:bg-creos font-sat before:absolute before:h-1 before:-bottom-1 before:transition-all before:duration-300 ${
+                    isActive
+                      ? 'text-creos/70 font-bold before:w-full'
+                      : 'text-[#E6EAE8] before:w-[0px] hover:before:w-full'
+                  } relative`}
+                >
+                  {route.route}
+                </div>
+              )}
+            </NavLink>
+          ))}
+        </ul>
+        <Button type='button' className='bg-[#E6EAE8] hover:bg-[#D9DFDD] '>
+          Join our community
+        </Button>
       </nav>
     </header>
   );
