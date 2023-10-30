822f6a1a10176dc8442b0d8ad98361eca0a590b5
Victor Olatunji <victoluolatunji@gmail.com>
2023-10-30 11:41:12 +0100

    tbc

--- src/layouts/Home/creatorsHaven.jsx
+++ src/layouts/Home/creatorsHaven.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { H3, Text } from '../../utils/typography';
 
 export default function CreatorsHaven() {
   return (
-    <section className='lg:px-[100px] lg:py-[120px] mx-[120px] my-[100px]'></section>
+    <section className='bg-white rounded-[100px] flex flex-col lg:px-[100px] lg:py-[120px] mx-[120px] my-[100px]'>
+      <Text>Creators’ Haven</Text>
+      <H3 className={`text-[#222222]`}>
+        At Creos, we believe in the extraordinary power of dreams. They are the
+        seeds from which innovations sprout, and at Creos, we provide the
+        fertile ground for those seeds to grow.
+      </H3>
+    </section>
   );
 }
--- src/layouts/Home/partner.jsx
+++ src/layouts/Home/partner.jsx
@@ -13,7 +13,7 @@ export default function Partner() {
   return (
     <section className='bg-white w-ful p-[100px] mb-[100px] flex flex-col items-center gap-10'>
       <H2 className={`text-[#222222] `}>Media Partners</H2>
-      <div className='flex flex-ro items-start justify-around gap-[79px]'>
+      <div className='flex flex-row flex-wrap lg:flex-nowrap items-start justify-around gap-[79px]'>
         <figure>
           <Bloomberg />
         </figure>
