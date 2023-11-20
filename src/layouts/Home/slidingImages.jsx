cf6348e0924614ac3d77b9b3c927d2dc2c500760
Victor Ola <victoluolatunji@gmail.com>
2023-11-21 00:50:46 +0100

    .

--- src/layouts/Home/slidingImages.jsx
+++ src/layouts/Home/slidingImages.jsx
@@ -11,7 +11,10 @@ export default function SlidingImages() {
           <section className='my-5'>
             <div className='flex  flex-row  gap-[28px] mx-4'>
               {imgSlider.map((img, index) => (
-                <figure key={index} className='outline-[#E6EAE8] w-[52px] lg:w-[120px] outline-[4px] lg:outline-[8px] outline rounded-[12px] lg:rounded-[26px] hover:scale-105'>
+                <figure
+                  key={index}
+                  className='outline-[#E6EAE8] w-[52px] lg:w-[120px] outline-[4px] lg:outline-[8px] outline rounded-[12px] lg:rounded-[26px] hover:scale-105'
+                >
                   <img
                     key={img.id}
                     alt={img.alt}
